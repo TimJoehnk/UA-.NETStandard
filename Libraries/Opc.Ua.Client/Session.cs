c7eb2a548273d2b2aaea8bcb5dc924a4dcbaf8c6
Martin Regen <mregen@microsoft.com>
2023-04-27 11:08:13 +0200

    Use ICloneable in generated code (#2044)
    - ICloneable was removed from generated code for the first port to .NET Standard 1.x, instead MemberwiseClone was used
    - Reenable ICloneable for generated code
    - Add ICloneable to built in types
    - Fix code where MemberwiseClone is called instead of Clone

--- Libraries/Opc.Ua.Client/Session.cs
+++ Libraries/Opc.Ua.Client/Session.cs
@@ -1543,7 +1543,7 @@ namespace Opc.Ua.Client
             ResponseHeader responseHeader = this.Read(
                 null,
                 0,
-                TimestampsToReturn.Both,
+                TimestampsToReturn.Neither,
                 nodesToRead,
                 out DataValueCollection values,
                 out DiagnosticInfoCollection diagnosticInfos);
